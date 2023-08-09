226c3760c06a7cfb95160bfaabfc2ecd45b6a47f
Marc Cataford <mcat@riseup.net>
2023-08-08 23:25:01 -0400

    feat: basic file create/read interactions

--- backend/rotini/api/files.py
+++ backend/rotini/api/files.py
@@ -5,39 +5,38 @@ This API allows users to create and query for existing data about
 files that live in the system.
 """
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, HTTPException, UploadFile
+
+import use_cases.files as files_use_cases
 
 router = APIRouter(prefix="/files")
-
-data = {
-    "123": {
-        "title": "Test file",
-        "filename": "testfile.txt",
-        "size": 1023,
-        "uid": "123",
-    },
-    "456": {
-        "title": "Other file",
-        "filename": "testfile2.txt",
-        "size": 535346,
-        "uid": "456",
-    },
-}
 
 
 @router.get("/")
 def list_files():
-    return list(data.values())
+    return files_use_cases.get_all_file_records()
 
 
 @router.post("/")
 async def upload_file(file: UploadFile):
+    content = await file.read()
+    size = len(content)
+    await file.seek(0)
+
     with open(file.filename, "wb") as f:
         content = await file.read()
         f.write(content)
-    return file.filename
+
+    created_record = files_use_cases.create_file_record(file.filename, size)
+
+    return created_record
 
 
 @router.get("/{file_id}/")
 def get_file_details(file_id: str):
-    return data[file_id]
+    file = files_use_cases.get_file_record_by_id(file_id)
+
+    if file is None:
+        return HTTPException(status_code=404)
+
+    return file
---
+++ backend/rotini/use_cases/files.py
@@ -0,0 +1,86 @@
+"""
+File-related use cases.
+
+Use cases and data structures defined in this file
+manipulate file records in the database or represent them
+after they have been read.
+"""
+import typing
+import pathlib
+
+from db import get_connection
+
+
+class FileRecord(typing.TypedDict):
+    """
+    Database record associated with a file tracked
+    by the system.
+    """
+
+    id: str
+    size: int
+    path: str
+    filename: str
+
+
+def create_file_record(path: str, size: int) -> FileRecord:
+    """
+    Creates a record representing an uploaded file in the database.
+
+    The record itself does not ensure that the file exists on disk, but just
+    that it's tracked by the system.
+    """
+    inserted_id = None
+
+    with get_connection() as connection, connection.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO files (path, size) VALUES (%s, %s) RETURNING id", (path, size)
+        )
+
+        inserted_id = cursor.fetchone()
+
+    filename = pathlib.Path(path).name
+
+    return FileRecord(id=inserted_id, size=size, path=path, filename=filename)
+
+
+def get_all_file_records() -> typing.Tuple[FileRecord]:
+    """
+    Fetches all availables files from the database.
+    """
+
+    rows = None
+
+    with get_connection() as connection, connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM files;")
+        rows = cursor.fetchall()
+
+    if rows is None:
+        raise RuntimeError("Failed to get files.")
+
+    return (
+        FileRecord(
+            id=row[0], path=row[1], size=row[2], filename=pathlib.Path(row[1]).name
+        )
+        for row in rows
+    )
+
+
+def get_file_record_by_id(file_id: str) -> typing.Optional[FileRecord]:
+    """
+    Fetches a single file by ID.
+
+    If the ID doesn't correspond to a record, None is returned.
+    """
+
+    row = None
+    with get_connection() as connection, connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM files WHERE id=%s;", (file_id,))
+        row = cursor.fetchone()
+
+    if row is None:
+        return None
+
+    return FileRecord(
+        id=row[0], path=row[1], size=row[2], filename=pathlib.Path(row[1]).name
+    )
