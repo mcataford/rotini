788d72c3830bc99778afcbe8b866ec67682e2f59
Marc Cataford <mcat@riseup.net>
2023-08-12 15:52:01 -0400

    feat(backend): split application configuration by env

--- backend/rotini/settings.py
+++ backend/rotini/settings.py
@@ -1,5 +1,12 @@
-DATABASE_USERNAME = "postgres"
-DATABASE_PASSWORD = "test"
-DATABASE_HOST = "localhost"
-DATABASE_PORT = 5431
-DATABASE_NAME = "postgres"
+# pylint: disable=unused-import, wildcard-import, unused-wildcard-import
+import os
+
+IS_CI = os.getenv("ROTINI_CI")
+IS_TEST = os.getenv("ROTINI_TEST")
+
+if IS_CI is not None:
+    from envs.ci import *
+elif IS_TEST is not None:
+    from envs.test import *
+else:
+    from envs.local import *
