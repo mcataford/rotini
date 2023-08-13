2a6b4a08345ddbe17536ae464f1930d92d02e320
Marc Cataford <mcat@riseup.net>
2023-08-13 17:05:54 -0400

    test(backend): add coverage for deletion + adapt coverage to only rely on endpoints

--- backend/tests/test_api_files.py
+++ backend/tests/test_api_files.py
@@ -1,23 +1,43 @@
-from rotini.use_cases import files as files_use_cases
+import pathlib
 
 
-def test_list_files_returns_registered_files_and_200(client):
-    file_1 = files_use_cases.create_file_record("/test1.txt", 123)
-    file_2 = files_use_cases.create_file_record("/test2.txt", 456)
+def test_list_files_returns_registered_files_and_200(client, tmp_path):
+    mock_file_1 = tmp_path / "test1.txt"
+    mock_file_1.write_text("testtest")
+
+    with open(str(mock_file_1), "rb") as mock_file_stream:
+        response = client.post("/files/", files={"file": mock_file_stream})
+
+    mock_file_1_data = response.json()
+
+    mock_file_2 = tmp_path / "test2.txt"
+    mock_file_2.write_text("testtest")
+
+    with open(str(mock_file_2), "rb") as mock_file_stream:
+        response = client.post("/files/", files={"file": mock_file_stream})
+
+    mock_file_2_data = response.json()
 
     response = client.get("/files/")
 
     assert response.status_code == 200
-    assert response.json() == [file_1, file_2]
+    assert response.json() == [mock_file_1_data, mock_file_2_data]
 
 
-def test_file_details_returns_specified_file_and_200(client):
-    file = files_use_cases.create_file_record("/test1.txt", 123)
+def test_file_details_returns_specified_file_and_200(client, tmp_path):
+    mock_file = tmp_path / "test.txt"
+    mock_file.write_text("testtest")
 
-    response = client.get(f"/files/{file['id']}/")
+    with open(str(mock_file), "rb") as mock_file_stream:
+        response = client.post("/files/", files={"file": mock_file_stream})
+
+    response_data = response.json()
+    created_file_id = response_data["id"]
+
+    response = client.get(f"/files/{created_file_id}/")
 
     assert response.status_code == 200
-    assert response.json() == file
+    assert response.json() == response_data
 
 
 def test_file_details_returns_404_if_does_not_exist(client):
@@ -25,3 +45,50 @@ def test_file_details_returns_404_if_does_not_exist(client):
     response = client.get(f"/files/{non_existent_id}/")
 
     assert response.status_code == 404
+
+
+def test_file_deletion_returns_404_if_does_not_exist(client):
+    non_existent_id = "06f02980-864d-4832-a894-2e9d2543a79a"
+    response = client.delete(f"/files/{non_existent_id}/")
+
+    assert response.status_code == 404
+
+
+def test_file_deletion_deletes_record_and_file(client, tmp_path):
+    mock_file = tmp_path / "test.txt"
+    mock_file.write_text("testtest")
+
+    with open(str(mock_file), "rb") as mock_file_stream:
+        response = client.post("/files/", files={"file": mock_file_stream})
+
+    response_data = response.json()
+    file_id = response_data["id"]
+    file_path = response_data["path"]
+
+    assert pathlib.Path(file_path).exists()
+    response = client.get(f"/files/{file_id}/")
+
+    assert response.status_code == 200
+
+    client.delete(f"/files/{file_id}/")
+    assert not pathlib.Path(file_path).exists()
+
+    response = client.get(f"/files/{file_id}/")
+
+    assert response.status_code == 404
+
+
+def test_file_deletion_200_and_return_deleted_resource(client, tmp_path):
+    mock_file = tmp_path / "test.txt"
+    mock_file.write_text("testtest")
+
+    with open(str(mock_file), "rb") as mock_file_stream:
+        response = client.post("/files/", files={"file": mock_file_stream})
+
+    response_data = response.json()
+    file_id = response_data["id"]
+
+    response = client.delete(f"/files/{file_id}/")
+
+    assert response.status_code == 200
+    assert response.json() == response_data
