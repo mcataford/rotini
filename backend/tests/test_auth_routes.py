bee2cc20119c2a79b7999c54d627ec55a5e9d455
Marc Cataford <mcat@riseup.net>
2023-08-20 11:07:13 -0400

    test(backend): login route coverage

---
+++ backend/tests/test_auth_routes.py
@@ -0,0 +1,73 @@
+import pytest
+
+
+@pytest.fixture(name="test_user_credentials")
+def fixture_test_user_creds():
+    """
+    Test user credentials.
+    """
+    return {"username": "testuser", "password": "testpassword"}
+
+
+@pytest.fixture(name="test_user", autouse=True)
+def fixture_test_user(client, test_user_credentials):
+    """
+    Sets up a test user using the `test_user_credentials` data.
+    """
+    response = client.post("/auth/users/", json=test_user_credentials)
+    yield response
+
+
+def test_create_user_returns_200_on_success():
+    pass
+
+
+def test_create_user_with_nonunique_username_fails():
+    pass
+
+
+def test_create_user_requires_username_and_password_supplied():
+    pass
+
+
+def test_log_in_returns_200_and_user_on_success(client, test_user_credentials):
+    # The `test_user` fixture creates a user.
+
+    response = client.post("/auth/sessions/", json=test_user_credentials)
+
+    assert response.status_code == 200
+
+    returned = response.json()
+
+    assert returned["username"] == test_user_credentials["username"]
+
+
+def test_log_in_returns_401_on_wrong_password(client, test_user_credentials):
+    response = client.post(
+        "/auth/sessions/",
+        json={"username": test_user_credentials["username"], "password": "sillystring"},
+    )
+
+    assert response.status_code == 401
+
+
+def test_log_in_returns_401_on_nonexistent_user(client):
+    response = client.post(
+        "/auth/sessions/", json={"username": "notauser", "password": "sillystring"}
+    )
+
+    assert response.status_code == 401
+
+
+@pytest.mark.parametrize(
+    "credentials",
+    [
+        pytest.param({"username": "test"}, id="username_only"),
+        pytest.param({"password": "test"}, id="password_only"),
+        pytest.param({}, id="no_data"),
+    ],
+)
+def test_log_in_returns_422_on_invalid_input(client, credentials):
+    response = client.post("/auth/sessions", json=credentials)
+
+    assert response.status_code == 422
