c0be967b3bd8b2a3845d782678391a0f41d3b2a2
Marc Cataford <mcat@riseup.net>
2023-08-27 11:22:34 -0400

    feat(backend): ensure that signature expiration is handled in middleware

--- backend/rotini/auth/middleware.py
+++ backend/rotini/auth/middleware.py
@@ -1,8 +1,19 @@
+"""
+Authentication & authorization middleware logic.
+
+This module is imported dynamically from `settings` to set up
+middlewares with the `FastAPI` singleton.
+"""
+import logging
+
+import jwt.exceptions
 from fastapi import Request
 
 from main import app
 
 import auth.use_cases as auth_use_cases
+
+logger = logging.getLogger(__name__)
 
 
 @app.middleware("http")
@@ -15,11 +26,17 @@ async def authentication_middleware(request: Request, call_next):
     authorization.
     """
     auth_header = request.headers.get("authorization")
+    decoded_token = None
 
     if auth_header is not None:
         _, token = auth_header.split(" ")
-        decoded_token = auth_use_cases.decode_token(token)
-        print(decoded_token)
+        try:
+            decoded_token = auth_use_cases.decode_token(token)
+        except jwt.exceptions.ExpiredSignatureError as exc:
+            logger.exception(exc)
+
+    if decoded_token is not None:
+        logger.info(decoded_token)
         request.state.user = {
             "username": decoded_token["username"],
             "user_id": decoded_token["user_id"],
