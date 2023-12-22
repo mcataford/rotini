81e83ddb6696964cccfebf860d13f6cc85ae8caa
Marc Cataford <mcat@riseup.net>
2023-12-21 23:31:54 -0500

    refactor: refactor filedetails + filelist into filelistview wrapper

---
+++ frontend/src/components/FileListView/index.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+
+import Box from "@mui/material/Box"
+
+import FileList from "../FileList"
+import FileDetails from "../FileDetails"
+import { useOwnFileList } from "../../hooks/files"
+import { useLocationContext } from "../../contexts/LocationContext"
+
+function FileListView() {
+	const { isLoading, data } = useOwnFileList()
+	const { location } = useLocationContext()
+
+	const sidebar = React.useMemo(() => {
+		if (location.params.itemId)
+			return (
+				<Box
+					aria-label="item details sidebar"
+					component="div"
+					sx={{ flexGrow: 1 }}
+				>
+					<FileDetails itemId={location.params.itemId} />
+				</Box>
+			)
+		return null
+	}, [location])
+
+	if (isLoading || !data) return "Loading"
+
+	return (
+		<>
+			<Box component="div" sx={{ flexGrow: 1 }}>
+				<FileList data={data} />
+			</Box>
+			{sidebar}
+		</>
+	)
+}
+
+export default FileListView
