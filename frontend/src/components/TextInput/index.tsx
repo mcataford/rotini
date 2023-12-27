d78c90d6549d59c5fea3ad24a4ccafa5230400fb
Marc Cataford <mcat@riseup.net>
2023-12-26 19:46:24 -0500

    feat(frontend): add TextInput component

---
+++ frontend/src/components/TextInput/index.tsx
@@ -0,0 +1,63 @@
+/*
+ * The TextInput component is a multipurpose text-input component
+ * with validation.
+ *
+ * TextInput is a controlled component and expects to be provided with
+ * a current value and state update function (onChange).
+ */
+
+import React from "react"
+import FormControl from "@mui/material/FormControl"
+import TextField from "@mui/material/TextField"
+import InputLabel from "@mui/material/InputLabel"
+import FormHelperText from "@mui/material/FormHelperText"
+
+interface Props {
+	// Aria label applied to the input element.
+	ariaLabel: string
+	// Text to display if validation fails. Only used if a validation function is provided.
+	errorText?: string
+	// Text label visible to the user with the input.
+	label: string
+	// Function to run on each field change.
+	onChange: (value: string) => void
+	// Optional validation function that decides if the current input state triggers an error.
+	validate?: (value: string) => boolean
+	// Input field value
+	value: string | undefined
+}
+
+function TextInput({
+	ariaLabel,
+	errorText,
+	label,
+	onChange,
+	validate = () => true,
+	value,
+}: Props) {
+	const isError = value !== undefined && !validate(value)
+
+	const helpText = isError ? <FormHelperText>{errorText}</FormHelperText> : null
+
+	return (
+		<FormControl>
+			<TextField
+				label={label}
+				value={value ?? ""}
+				onChange={(e) => {
+					const updatedValue = e.target.value
+					onChange(updatedValue)
+				}}
+				error={isError}
+				inputProps={{
+					"aria-label": ariaLabel,
+				}}
+			/>
+			{helpText}
+		</FormControl>
+	)
+}
+
+export { Props as TextInputProps }
+
+export default TextInput
