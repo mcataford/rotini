b446d17c4ffea55a35663fa4fe30ab7d3946a0d7
Marc Cataford <mcat@riseup.net>
2023-12-26 20:37:59 -0500

    feat(frontend): add RegisterView + router

---
+++ frontend/src/components/RegisterView/RegisterView.test.tsx
@@ -0,0 +1,74 @@
+import { screen, render } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import AxiosMockAdapter from "axios-mock-adapter"
+
+import axios from "../../axios"
+import RegisterView from "."
+
+function renderComponent() {
+	return {
+		...render(
+			<QueryClientProvider client={new QueryClient()}>
+				<RegisterView />
+			</QueryClientProvider>,
+		),
+		user: userEvent.setup(),
+	}
+}
+
+describe("RegisterView", () => {
+	it("renders form fields", () => {
+		renderComponent()
+
+		expect(screen.queryByLabelText("Email")).toBeInTheDocument()
+		expect(
+			screen.queryByLabelText("New account email address"),
+		).toBeInTheDocument()
+
+		expect(screen.queryByLabelText("Password")).toBeInTheDocument()
+		expect(
+			screen.queryByLabelText("New account password input"),
+		).toBeInTheDocument()
+	})
+
+	it("renders a submission button", () => {
+		renderComponent()
+
+		expect(
+			screen.queryByLabelText("submit account registration"),
+		).toBeInTheDocument()
+		expect(screen.queryByText("Create account")).toBeInTheDocument()
+	})
+
+	it("sends a request to the account creation API on submission", async () => {
+		const axiosMockAdapter = new AxiosMockAdapter(axios)
+
+		axiosMockAdapter
+			.onPost("/auth/user/")
+			.reply(201, { username: "test", id: 1 })
+
+		const { user } = renderComponent()
+
+		const testInput = {
+			username: "test@domain.com",
+			password: "password",
+		}
+
+		const emailInput = screen.getByLabelText("New account email address")
+		await user.type(emailInput, testInput.username)
+
+		const passwordInput = screen.getByLabelText("New account password input")
+		await user.type(passwordInput, testInput.password)
+
+		const submitButton = screen.getByText("Create account")
+
+		await user.click(submitButton)
+
+		expect(axiosMockAdapter.history.post.length).toEqual(1)
+
+		const requestBody = JSON.parse(axiosMockAdapter.history.post[0].data)
+
+		expect(requestBody).toEqual(testInput)
+	})
+})
---
+++ frontend/src/components/RegisterView/index.tsx
@@ -0,0 +1,88 @@
+import React from "react"
+import { useMutation } from "@tanstack/react-query"
+
+import Typography from "@mui/material/Typography"
+import Box from "@mui/material/Box"
+import FormGroup from "@mui/material/FormGroup"
+import FormControl from "@mui/material/FormControl"
+import TextField from "@mui/material/TextField"
+import InputLabel from "@mui/material/InputLabel"
+import FormHelperText from "@mui/material/FormHelperText"
+import Button from "@mui/material/Button"
+
+import axiosWithDefaults from "../../axios"
+import TextInput from "../TextInput"
+
+function RegisterView() {
+	const [emailAddress, setEmailAddress] = React.useState<string | undefined>()
+	const [password, setPassword] = React.useState<string | undefined>()
+
+	const { mutate } = useMutation({
+		mutationFn: async ({
+			email,
+			password,
+		}: { email: string; password: string }) => {
+			const response = await axiosWithDefaults.post("/auth/user/", {
+				username: email,
+				password,
+			})
+
+			return response
+		},
+	})
+
+	const emailField = React.useMemo(
+		() => (
+			<TextInput
+				errorText={"Enter valid email of the format 'abc@xyz.org'"}
+				label="Email"
+				ariaLabel="New account email address"
+				onChange={setEmailAddress}
+				value={emailAddress}
+			/>
+		),
+		[emailAddress, setEmailAddress],
+	)
+
+	const passwordField = React.useMemo(
+		() => (
+			<TextInput
+				errorText={"A valid password should be valid."}
+				label="Password"
+				ariaLabel="New account password input"
+				onChange={setPassword}
+				value={password}
+			/>
+		),
+		[setPassword, password],
+	)
+
+	const onCreateClick = React.useCallback(() => {
+		if (!emailAddress || !password) return
+		mutate({ email: emailAddress, password: password })
+	}, [mutate, emailAddress, password])
+
+	return (
+		<Box sx={{ display: "flex", justifyContent: "center", width: "100%" }}>
+			<FormGroup sx={{ flexGrow: 0.1, display: "flex", gap: "10px" }}>
+				<Typography variant="h1" sx={{ fontSize: "2rem" }}>
+					Create an account
+				</Typography>
+				<Typography>
+					{"Fill the form below to create an account and get started!"}
+				</Typography>
+				{emailField}
+				{passwordField}
+				<Button
+					variant="contained"
+					onClick={onCreateClick}
+					aria-label="submit account registration"
+				>
+					Create account
+				</Button>
+			</FormGroup>
+		</Box>
+	)
+}
+
+export default RegisterView
