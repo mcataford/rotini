1648e0995554749b974211f4f0733f27900dbb0b
Marc Cataford <mcat@riseup.net>
2023-12-27 20:51:10 -0500

    fix(be-start): ensure that app + db are networked correctly

--- backend/script/start.sh
+++ backend/script/start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+docker network create rotini-local || echo "Network already exists"
+
 docker run \
     --name $DB_CONTAINER_NAME \
-    -e POSTGRES_PASSWORD=$DATABASE_PASSWORD \
-    -e POSTGRES_USER=$DATABASE_USER \
-    -e POSTGRES_DB=$DATABASE_NAME \
+    -e POSTGRES_PASSWORD=$DB_PASSWORD \
+    -e POSTGRES_USER=$DB_USER \
+    -e POSTGRES_DB=$DB_NAME \
     -v $DATABASE_STORAGE_PATH:/var/lib/postgresql/data \
     -p 5432:5432 \
+    --network rotini-local \
     -d \
     postgres:15.4
 
@@ -20,4 +23,5 @@ docker run \
     --publish 8000:8000 \
     --name $APP_CONTAINER_NAME \
     --env-file ../backend.env \
+    --network rotini-local \
     rotini:dev
